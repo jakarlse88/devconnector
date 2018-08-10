21f14d97fc073b6c11f6e8e296555cfa4601894d
Jon Karlsen <karlsen.jonarild@gmail.com>
2018-08-10 16:40:38 +0200

    connect to mongoDB w/ Mongoose

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to mongoDB
+mongoose
+    .connect(db)
+    .then(() => console.log('mongoDB connected'))
+    .catch(e => console.log(e));
 
 app.get('/', (req, res) => res.send('heya whirl'));
 
