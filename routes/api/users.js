7c7aa017d3119951123987df7492e41543f804f4
Jon Karlsen <karlsen.jonarild@gmail.com>
2018-08-12 11:53:14 +0200

    Implement /api/users/login

--- routes/api/users.js
+++ routes/api/users.js
@@ -54,4 +54,41 @@ router.post('/register', (req, res) => {
     })
 });
 
+// @route   POST api/users/login
+// @desc    Login user / returning token
+// @access  Public
+router.post('/login', (req, res) => {
+    const {
+        email,
+        password
+    } = req.body;
+
+    // Find user by email
+    User.findOne({
+            email
+        })
+        .then(user => {
+            // Check for user
+            if (!user) {
+                return res.status(404).json({
+                    email: 'User not found'
+                });
+            }
+
+            // Check password
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        res.json({
+                            msg: 'success'
+                        });
+                    } else {
+                        return res.status(400).json({
+                            password: 'Incorrect password'
+                        })
+                    }
+                })
+        })
+});
+
 module.exports = router;
