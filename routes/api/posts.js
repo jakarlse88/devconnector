9d8750835b896672176e07538dbdf1ce0c74c126
Jon Karlsen <karlsen.jonarild@gmail.com>
2018-08-20 10:40:35 +0200

    Implement GET /posts/ /posts/:id, DELETE /post/:id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,9 @@ const passport = require('passport');
 // Load Post model
 const Post = require('../../models/Post');
 
+// Load Profile model
+const Profile = require('../../models/Profile');
+
 // Load validation
 const validatePostInput = require('../../validation/post');
 
@@ -19,7 +22,7 @@ router.get('/test', (req, res) => {
 });
 
 // @route   GET api/posts
-// @desc    Get posts
+// @desc    Get all posts
 // @access  Public
 router.get('/', (req, res) => {
     Post.find()
@@ -27,7 +30,20 @@ router.get('/', (req, res) => {
             date: -1
         })
         .then(posts => res.json(posts))
-        .catch(err => res.status(404));
+        .catch(err => res.status(404).json({
+            nopostsfound: 'No posts found'
+        }));
+});
+
+// @route   GET api/posts/:id
+// @desc    Get post by ID
+// @access  Public
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id)
+        .then(post => res.json(post))
+        .catch(err => res.status(404).json({
+            nopostfound: 'No post found with that ID'
+        }));
 });
 
 // @route   POST api/posts
@@ -60,6 +76,45 @@ router.post('/', passport.authenticate('jwt', {
             errors.savepost = err;
             return res.status(500).json(errors);
         });
+});
+
+// @route   DELETE api/posts/:id
+// @desc    Delete a post by ID
+// @access  Private
+router.delete('/:id', passport.authenticate('jwt', {
+    session: false
+}), (req, res) => {
+    // Hold our errors
+    const errors = {};
+
+    Profile.findOne({
+            user: req.user.id
+        })
+        .then(profile => {
+            Post.findById(req.params.id)
+                .then(post => {
+                    // Check post owner
+                    if (post.user.toString() !== req.user.id) {
+                        erorrs.notauthorised = 'User not authorised';
+                        return res.status(401).json(errors);
+                    }
+
+                    // Delete
+                    post.remove()
+                        .then(() => res.json({
+                            success: true
+                        }))
+                        .catch(err => {
+                            errors.postnotfound = 'Post not found';
+                            return res.status(404).json(errors);
+                        })
+                })
+                .catch(err => {});
+        })
+        .catch(err => {
+            errors.profilefind = 'Couldn\'t find a profile with specified ID';
+            return res.status(500).json(errors);
+        })
 });
 
 
