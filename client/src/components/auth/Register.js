97fe5966901b443f021c524d4e4cb968d84bd7aa
Jon Karlsen <karlsen.jonarild@gmail.com>
2018-08-22 13:58:02 +0200

    Setup React Router

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Reigster extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Register</h1>
+      </div>
+    );
+  }
+}
+
+export default Reigster;
