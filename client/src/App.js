e8ec3fb2107813bca3537b4fe2c84290812f4e20
Jon Karlsen <karlsen.jonarild@gmail.com>
2018-08-22 23:26:12 +0200

    Implement registration (no redux)

--- client/src/App.js
+++ client/src/App.js
@@ -10,21 +10,21 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-          </div>
-          <Footer />
-        </div>
-      </Router>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<div className="App">
+					<Navbar />
+					<Route exact path="/" component={Landing} />
+					<div className="container">
+						<Route exact path="/register" component={Register} />
+						<Route exact path="/login" component={Login} />
+					</div>
+					<Footer />
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
