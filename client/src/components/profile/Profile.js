b0b045bc3d27ad47643899f31b6e889d0185d574
Jon Karlsen <karlsen.jonarild@gmail.com>
2018-08-27 12:56:18 +0200

    Implement ProfileCreds comp

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -13,63 +13,64 @@ import Spinner from '../common/Spinner';
 import { getProfileByHandle } from '../../actions/profileActions';
 
 class Profile extends Component {
-    componentDidMount = () => {
-        if (this.props.match.params.handle) {
-            this.props.getProfileByHandle(this.props.match.params.handle);
-        }
-    };
-
-    render() {
-        const { profile, loading } = this.props.profile;
-
-        let profileContent;
-
-        if (profile === null || loading) {
-            profileContent = <Spinner />;
-        } else {
-            profileContent = (
-                <div>
-                    <div className="row">
-                        <div className="col-md-6">
-                            <Link
-                                to="/profiles"
-                                className="btn btn-light mb-3 float-left">
-                                Back to profiles
-                            </Link>
-                        </div>
-                        <div className="col-md-6" />
-                    </div>
-                    <ProfileHeader profile={profile} />
-                    <ProfileAbout profile={profile} />
-                    <ProfileCreds education={profile.education} experience={profile.experience} />
-                    <ProfileGitHub />
-                </div>
-            );
-        }
-
-        return (
-            <div className="profile">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12">{profileContent}</div>
-                    </div>
-                </div>
-            </div>
-        );
+  componentDidMount = () => {
+    if (this.props.match.params.handle) {
+      this.props.getProfileByHandle(this.props.match.params.handle);
     }
+  };
+
+  render() {
+    const { profile, loading } = this.props.profile;
+
+    let profileContent;
+
+    if (profile === null || loading) {
+      profileContent = <Spinner />;
+    } else {
+      profileContent = (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <Link to="/profiles" className="btn btn-light mb-3 float-left">
+                Back to profiles
+              </Link>
+            </div>
+            <div className="col-md-6" />
+          </div>
+          <ProfileHeader profile={profile} />
+          <ProfileAbout profile={profile} />
+          <ProfileCreds
+            education={profile.education}
+            experience={profile.experience}
+          />
+          <ProfileGitHub />
+        </div>
+      );
+    }
+
+    return (
+      <div className="profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">{profileContent}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 Profile.propTypes = {
-    getProfileByHandle: PropTypes.func.isRequired,
-    profile: PropTypes.object.isRequired
+  getProfileByHandle: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-    getProfileByHandle: state.getProfileByHandle,
-    profile: state.profile
+  getProfileByHandle: state.getProfileByHandle,
+  profile: state.profile
 });
 
 export default connect(
-    mapStateToProps,
-    { getProfileByHandle }
+  mapStateToProps,
+  { getProfileByHandle }
 )(Profile);
